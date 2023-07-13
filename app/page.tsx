4ef222c4f992c08f9e0918de1b4f137a6d9c3447
GNVageesh <vageeshgn2005@gmail.com>
2023-07-13 14:46:46 +0530

    :lipstick: Some UI changes to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
 import Link from "next/link";
+import { FaGithub, FaHeart } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1 className="text-4xl font-semibold">Quizo</h1>
-        <Link href="/quiz">
-          <button className="bg-green-500 hover:bg-green-700 py-1 px-2 text-xl rounded-lg mt-2">
-            Start
-          </button>
+    <div>
+      <nav>
+        <Link href="https://github.com/cryskram/quizo">
+          <FaGithub className="text-2xl" />
         </Link>
-      </div>
-    </main>
+      </nav>
+      <main className="w-full flex-col text-center min-h-screen flex justify-center items-center">
+        <div>
+          <h1 className="text-6xl font-semibold font-mono">Quizo</h1>
+          <p className="font-mono text-slate-400">
+            A simple quiz! Check it out
+          </p>
+          <Link href="/quiz">
+            <button className="bg-green-500 hover:bg-green-700 py-1 px-2 text-2xl rounded-lg mt-2">
+              Start Quiz
+            </button>
+          </Link>
+        </div>
+        <p className="absolute bottom-0 flex items-center">
+          Made with
+          <span>
+            <FaHeart className="text-red-600 mx-2" />
+          </span>
+          by cryskram
+        </p>
+      </main>
+    </div>
   );
 }
