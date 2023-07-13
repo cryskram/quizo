fc8f762b046e1ac88ceab732a25a3f11d6baf41c
CrysKram <65950037+cryskram@users.noreply.github.com>
2023-07-13 23:01:55 +0530

    New questions data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,85 +1,96 @@
 export const quiz = {
-  totalQuestions: 10,
+  totalQuestions: 15,
   questions: [
     {
-      id: 1,
-      question: "What is the capital of France?",
-      answers: ["London", "Paris", "Rome", "Berlin"],
-      correctAnswer: "Paris",
+      id: 11,
+      question: "Which country is home to the Great Barrier Reef?",
+      answers: ["Australia", "Brazil", "Mexico", "India"],
+      correctAnswer: "Australia"
     },
     {
-      id: 2,
-      question: "Which planet is known as the 'Red Planet'?",
-      answers: ["Mars", "Venus", "Jupiter", "Mercury"],
-      correctAnswer: "Mars",
+      id: 12,
+      question: "Who wrote the novel 'To Kill a Mockingbird'?",
+      answers: ["F. Scott Fitzgerald", "Harper Lee", "Jane Austen", "George Orwell"],
+      correctAnswer: "Harper Lee"
     },
     {
-      id: 3,
-      question: "Who painted the Mona Lisa?",
-      answers: [
-        "Leonardo da Vinci",
-        "Pablo Picasso",
-        "Vincent van Gogh",
-        "Michelangelo",
-      ],
-      correctAnswer: "Leonardo da Vinci",
+      id: 13,
+      question: "Which city hosted the 2016 Summer Olympics?",
+      answers: ["Tokyo", "Rio de Janeiro", "Sydney", "Beijing"],
+      correctAnswer: "Rio de Janeiro"
     },
     {
-      id: 4,
-      question: "What is the chemical symbol for gold?",
-      answers: ["Ag", "Au", "Hg", "Fe"],
-      correctAnswer: "Au",
+      id: 14,
+      question: "What is the largest ocean on Earth?",
+      answers: ["Atlantic Ocean", "Arctic Ocean", "Indian Ocean", "Pacific Ocean"],
+      correctAnswer: "Pacific Ocean"
     },
     {
-      id: 5,
-      question: "Which country won the FIFA World Cup in 2018?",
-      answers: ["Brazil", "France", "Germany", "Spain"],
-      correctAnswer: "France",
+      id: 15,
+      question: "In which year did World War II end?",
+      answers: ["1942", "1945", "1950", "1939"],
+      correctAnswer: "1945"
     },
     {
-      id: 6,
-      question: "Who wrote the play 'Romeo and Juliet'?",
-      answers: [
-        "William Shakespeare",
-        "Charles Dickens",
-        "Mark Twain",
-        "Jane Austen",
-      ],
-      correctAnswer: "William Shakespeare",
+      id: 16,
+      question: "Who is the artist behind the famous painting 'The Starry Night'?",
+      answers: ["Vincent van Gogh", "Salvador Dalí", "Pablo Picasso", "Leonardo da Vinci"],
+      correctAnswer: "Vincent van Gogh"
     },
     {
-      id: 7,
-      question: "What is the tallest mountain in the world?",
-      answers: [
-        "Mount Kilimanjaro",
-        "Mount Everest",
-        "Mount McKinley",
-        "Mount Fuji",
-      ],
-      correctAnswer: "Mount Everest",
+      id: 17,
+      question: "Which planet is known as the 'Giant Red Spot'?",
+      answers: ["Saturn", "Neptune", "Jupiter", "Uranus"],
+      correctAnswer: "Jupiter"
     },
     {
-      id: 8,
-      question: "Which animal is known as the 'King of the Jungle'?",
-      answers: ["Lion", "Tiger", "Leopard", "Elephant"],
-      correctAnswer: "Lion",
+      id: 18,
+      question: "Which African country is known as the 'Land of the Pharaohs'?",
+      answers: ["Morocco", "Egypt", "Nigeria", "South Africa"],
+      correctAnswer: "Egypt"
     },
     {
-      id: 9,
-      question: "What is the largest organ in the human body?",
-      answers: ["Heart", "Liver", "Brain", "Skin"],
-      correctAnswer: "Skin",
+      id: 19,
+      question: "Who is the author of the 'Harry Potter' book series?",
+      answers: ["J.R.R. Tolkien", "J.K. Rowling", "George R.R. Martin", "Stephen King"],
+      correctAnswer: "J.K. Rowling"
     },
     {
-      id: 10,
-      question: "Which scientist developed the theory of relativity?",
-      answers: [
-        "Isaac Newton",
-        "Albert Einstein",
-        "Galileo Galilei",
-        "Nikola Tesla",
-      ],
-      correctAnswer: "Albert Einstein",
+      id: 20,
+      question: "Which famous scientist developed the theory of gravity?",
+      answers: ["Isaac Newton", "Albert Einstein", "Stephen Hawking", "Galileo Galilei"],
+      correctAnswer: "Isaac Newton"
     },
-  ],
+    {
+      id: 21,
+      question: "What is the world's largest desert?",
+      answers: ["Sahara Desert", "Gobi Desert", "Arabian Desert", "Kalahari Desert"],
+      correctAnswer: "Sahara Desert"
+    },
+    {
+      id: 22,
+      question: "Which instrument has 88 keys?",
+      answers: ["Guitar", "Violin", "Drums", "Piano"],
+      correctAnswer: "Piano"
+    },
+    {
+      id: 23,
+      question: "Which country is known as the 'Land of the Rising Sun'?",
+      answers: ["China", "Japan", "South Korea", "Thailand"],
+      correctAnswer: "Japan"
+    },
+    {
+      id: 24,
+      question: "Who painted the ceiling of the Sistine Chapel?",
+      answers: ["Raphael", "Leonardo da Vinci", "Donatello", "Michelangelo"],
+      correctAnswer: "Michelangelo"
+    },
+    {
+      id: 25,
+      question: "Which gas makes up the majority of Earth's atmosphere?",
+      answers: ["Nitrogen", "Oxygen", "Carbon Dioxide", "Hydrogen"],
+      correctAnswer: "Nitrogen"
+    }
+  ]
 };
+                
